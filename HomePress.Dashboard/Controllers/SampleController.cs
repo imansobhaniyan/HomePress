fdcd49965b0af3373db6cca669e08815d68f9eb1
farhad pc <farhad@pc.com>
2022-02-06 21:50:37 +0330

    Table Samples

---
+++ HomePress.Dashboard/Controllers/SampleController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HomePress.Dashboard.Controllers
+{
+    public class SampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Table()
+        {
+            return View();
+        }
+
+    }
+}
